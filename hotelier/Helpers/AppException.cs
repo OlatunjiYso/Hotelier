dd2800b825237c255de84846fc408b69ccb1a1f3
Olatunji Yusuf <olatunjiyso@gmail.com>
2021-04-07 09:57:17 +0100

    add custom error handlers

---
+++ hotelier/Helpers/AppException.cs
@@ -0,0 +1,11 @@
+
+using System;
+using System.Globalization;
+
+namespace hotelier.Helpers {
+    public class AppException: Exception {
+       public AppException() :base() {}
+       public AppException(string message) : base(message){}
+       public AppException(string message, params object[] args) :base(String.Format(CultureInfo.CurrentCulture, message, args)) {}
+    }
+}
